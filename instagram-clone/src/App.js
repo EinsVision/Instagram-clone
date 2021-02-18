a6d6d7e7384d2ca2c8792e2e8add64069df45f63
EinsVision <einsvision@gmail.com>
2021-02-19 00:15:33 +0900

    use material ui Modal

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -2,8 +2,37 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './Post';
 import {db} from './firebase';
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core';
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
   const [posts, setPosts] = useState([
     // {
     //   username : 'einsvision', 
@@ -21,6 +50,7 @@ function App() {
     //   imageUrl : 'https://wwmt.com/resources/media/3621b7b3-8e3e-4167-9e38-da94fa9ca069-large16x9_AP20190736080736.jpg?1594337025526'
     // }
   ]);
+  const [open, setOpen] = useState(false);
   
   // useEffect -> runs a piece of code based on a specific condition
   
@@ -36,6 +66,15 @@ function App() {
   
   return (
     <div className="app">
+      <Modal
+        open={open}
+        onClose={ () => setOpen(false) }
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <h2>I am a model</h2>
+        </div>
+      </Modal>
+
       <div className='app__header'>
         <img  className='app__headerImage'
               src='https://logodownload.org/wp-content/uploads/2017/04/instagram-logo-17.png' 
