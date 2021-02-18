2016e768d5ce8865721b357e100e77b94180b7a9
EinsVision <einsvision@gmail.com>
2021-02-18 20:47:55 +0900

    Posts using React hook and map function

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -3,7 +3,23 @@ import './App.css';
 import Post from './Post';
 
 function App() {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([
+    {
+      username : 'einsvision', 
+      caption: 'WOW it works.', 
+      imageUrl : 'https://hackernoon.com/images/z2xg2bpo.jpg'
+    },
+    {
+      username:'jcdlove', 
+      caption:'Instagram clone coding.', 
+      imageUrl : 'https://merriam-webster.com/assets/mw/images/article/art-wap-article-main/family-of-four-7101-90ebbf35ea8e9b9d264dbb2ab724bc4e@1x.jpg'
+    },
+    {
+      username:'messi', 
+      caption:'Soccer is my life.', 
+      imageUrl : 'https://wwmt.com/resources/media/3621b7b3-8e3e-4167-9e38-da94fa9ca069-large16x9_AP20190736080736.jpg?1594337025526'
+    }
+  ]);
   
   return (
     <div className="app">
@@ -13,9 +29,13 @@ function App() {
               alt=''
         />
       </div>
-      <Post username='einsvision' caption='WOW it works.' imageUrl='https://hackernoon.com/images/z2xg2bpo.jpg'/>
-      <Post username='jcdlove' caption='Instagram clone coding.'/>
-      <Post username='egoing' caption='i am a helper in the world.'/>
+
+      {
+        posts.map(post => (
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+        ))
+      }
+      
     </div>
   );
 }
