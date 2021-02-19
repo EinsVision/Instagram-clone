6fa1cf43e6d83a055a4fb33d976804806afd6f3a
EinsVision <einsvision@gmail.com>
2021-02-19 16:14:00 +0900

    added comment UI

--- instagram-clone/src/Post.js
+++ instagram-clone/src/Post.js
@@ -4,19 +4,30 @@ import { Avatar } from '@material-ui/core';
 import { db } from './firebase';
 
 function Post({postId, username, caption, imageUrl}) {
-  const [comments, setComments] = useState();
+  const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState('');
 
-  useEffect(()=>{
+  useEffect( () => {
     let unsubscribe;
-    if(postId){
-      unsubscribe = db.collection('posts')
-                      .doc(postId)
-                      .collection('comments')
-                      .onSnapshot((snapshot) => {
-                        setComments(snapshot.docs.map((doc)=>doc.data()));
-                      });
+    if ( postId ) {
+      unsubscribe = db
+      .collection('posts')
+      .doc(postId)
+      .collection('comments')
+      .onSnapshot((snapshot) => {
+        setComments(snapshot.docs.map( (doc)=>doc.data() ) );
+      });
     }
-  })
+
+    return () => {
+      unsubscribe();
+    };
+
+  }, [postId]);
+
+  const postComment = (event) => {
+
+  }
 
   return (
     <div className='post'>
@@ -28,18 +39,32 @@ function Post({postId, username, caption, imageUrl}) {
         />
         <h3>{username}</h3>
       </div>
-      
-      {/* header -> avatar + username */}
 
       <img
         className='post__image'
         src={imageUrl} 
         alt=''
       />
-      {/* image */}
-
-      {/* username + caption */}
+      
       <h4 className='post__text'><strong>{username}</strong> {caption}</h4>
+
+      <form>
+        <input
+          className='post__input'
+          type='text'
+          placeholder='Add a comment...'
+          value={comment} 
+          onChange={(e) => setComment(e.target.value)}
+        />
+        <button
+          disabled={!comment}
+          className='post__button'
+          type='submit'
+          onClick={postComment}
+        >
+          Post
+        </button>
+      </form>
     </div>
   )
 }
