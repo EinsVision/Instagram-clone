71c3fb37f169be521c2fa71042214aeee48a9fcb
EinsVision <einsvision@gmail.com>
2021-02-19 10:43:18 +0900

    Modal debugging

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -4,10 +4,10 @@ import Post from './Post';
 import {db} from './firebase';
 import Modal from '@material-ui/core/Modal';
 import { Button, Input, makeStyles } from '@material-ui/core';
-// import {makeStyles} from '@material-ui/core/styles';
+
 function getModalStyle() {
-  const top = 0;
-  const left = 0;
+  const top = 50;
+  const left = 50;
 
   return {
     top: `${top}%`,
@@ -17,12 +17,9 @@ function getModalStyle() {
 }
 
 const useStyles = makeStyles((theme) => ({
-  modal: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   paper: {
+    position: 'absolute',
+    width: 400,
     backgroundColor: theme.palette.background.paper,
     border: '2px solid #000',
     boxShadow: theme.shadows[5],
@@ -33,32 +30,13 @@ const useStyles = makeStyles((theme) => ({
 function App() {
   const classes = useStyles();
   const [modalStyle] = useState(getModalStyle);
-  const [posts, setPosts] = useState([
-    // {
-    //   username : 'einsvision', 
-    //   caption: 'WOW it works.', 
-    //   imageUrl : 'https://hackernoon.com/images/z2xg2bpo.jpg'
-    // },
-    // {
-    //   username:'jcdlove', 
-    //   caption:'Instagram clone coding.', 
-    //   imageUrl : 'https://merriam-webster.com/assets/mw/images/article/art-wap-article-main/family-of-four-7101-90ebbf35ea8e9b9d264dbb2ab724bc4e@1x.jpg'
-    // },
-    // {
-    //   username:'messi', 
-    //   caption:'Soccer is my life.', 
-    //   imageUrl : 'https://wwmt.com/resources/media/3621b7b3-8e3e-4167-9e38-da94fa9ca069-large16x9_AP20190736080736.jpg?1594337025526'
-    // }
-  ]);
+  const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
 
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  
-  
-  // useEffect -> runs a piece of code based on a specific condition
-  
+
   useEffect(() => {
     db.collection('posts').onSnapshot(snapshot => {
       // every time a new post is added in this code
@@ -80,7 +58,7 @@ function App() {
         onClose={ () => setOpen(false) }
       >
         <div style={modalStyle} className={classes.paper}>
-          <form>
+          <form className='app_signup'>
             <center>
               <img  className='app__headerImage'
                   src='https://logodownload.org/wp-content/uploads/2017/04/instagram-logo-17.png' 
